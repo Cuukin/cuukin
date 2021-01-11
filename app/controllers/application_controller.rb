03e618aee83f8a29d3998b0c8221dcc157574adf
Julia Frederico <magalhaesjulia@id.uff.br>
2021-01-11 10:26:36 -0300

    set up parameter sanitizer with new fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  # White list approach - every route needs authentication
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    # adding fields at sign up - devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # adding fields at edit profile - devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
