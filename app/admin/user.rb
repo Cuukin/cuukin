ed9265cd3fe9eb55d9809e5fa4826adf5fdef4db
Julia Frederico <magalhaesjulia@id.uff.br>
2021-01-13 20:39:54 -0300

    added users view to admin dashboard

---
+++ app/admin/user.rb
@@ -0,0 +1,12 @@
+ActiveAdmin.register User do
+  index do
+    selectable_column
+    column :id
+    column :email
+    column :first_name
+    column :last_name
+    column :created_at
+    column :admin
+    actions
+  end
+end
