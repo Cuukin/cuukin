301b85e8f315dbc839e13dfa82ecae16454c1034
Julia Frederico <magalhaesjulia@id.uff.br>
2021-01-15 13:49:22 -0300

    password visibility toggle javascript

---
+++ app/javascript/components/password_visibility_toggle.js
@@ -0,0 +1,14 @@
+const togglePasswordVisibility = () => {
+  const passwordToggle = document.querySelector('#toggle-password');
+  const password = document.querySelector('#my-password');
+
+  if (passwordToggle) {
+    passwordToggle.addEventListener('click', (event) => {
+      const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+      password.setAttribute('type', type);
+      passwordToggle.classList.toggle('fa-eye-slash');
+    });
+  }
+}
+
+export { togglePasswordVisibility };
