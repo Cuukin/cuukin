9420e2f13e8964a9e1255695a181ab2f8bf324f1
Julia Frederico <magalhaesjulia@id.uff.br>
2021-01-15 16:56:56 -0300

    added pundit policies for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,19 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  include Pundit
+
+  # Pundit: white-list approach.
+  # after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  # Uncomment when you *really understand* Pundit!
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
+
   private
 
   def configure_permitted_parameters
@@ -10,6 +23,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
 
     # adding fields at edit profile - devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+  end
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,18 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Your profile has been updated.'
+    else
+      render :edit
+    end
   end
 
   private
 
   def find_user
     @user = User.find(params[:id])
+    authorize @user
   end
 
   def user_params
