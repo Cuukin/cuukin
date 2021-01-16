b5c6383157f8858c1ac07b7aa8b535e017506e28
Julia Frederico <magalhaesjulia@id.uff.br>
2021-01-15 21:59:19 -0300

    created a job to perform avatar generator assynchronus

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,9 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
   validates :username, uniqueness: true
 
-  after_create :set_username, :set_avatar
+  after_create :set_username
   before_update :check_username_presence
+  after_create :async_set_avatar
 
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
@@ -33,19 +34,7 @@ class User < ApplicationRecord
     set_username if !(self.username) || self.username == ""
   end
 
-  def randomize_avatar
-    av1 = "https://res.cloudinary.com/cuukin/image/upload/v1610757107/Cuukin%20-%20Avatar/ck_av_9_bllcye.png"
-    av2 = "https://res.cloudinary.com/cuukin/image/upload/v1610757107/Cuukin%20-%20Avatar/ck_av_8_k6jxvr.png"
-    av3 = "https://res.cloudinary.com/cuukin/image/upload/v1610757107/Cuukin%20-%20Avatar/ck_av_7_dkpysc.png"
-    av4 = "https://res.cloudinary.com/cuukin/image/upload/v1610757106/Cuukin%20-%20Avatar/ck_av_5_cjcneu.png"
-    av5 = "https://res.cloudinary.com/cuukin/image/upload/v1610757107/Cuukin%20-%20Avatar/ck_av_10_q5t9ky.png"
-    av6 = "https://res.cloudinary.com/cuukin/image/upload/v1610757107/Cuukin%20-%20Avatar/ck_av_12_pyotlq.png"
-    [av1, av2, av3, av4, av5, av6].sample
-  end
-
-  def set_avatar
-    require "open-uri"
-    new_avatar = URI.open("#{randomize_avatar}")
-    self.photo.attach(io: new_avatar, filename: 'avatar.png', content_type: 'image/png')
+  def async_set_avatar
+    SetAvatarJob.perform_later(self)
   end
 end
