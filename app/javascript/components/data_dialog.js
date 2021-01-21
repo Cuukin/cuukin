4bcc61d37848b76e64cf85f725dfd91cca22cbf9
Julia Frederico <magalhaesjulia@id.uff.br>
2021-01-21 18:15:40 -0300

    taken out sweet alert gems and packages

---
+++ app/javascript/components/data_dialog.js
@@ -0,0 +1,8 @@
+Swal.fire({
+  title: 'Are you sure?',
+  text: 'You will not be able to recover your account 😢',
+  icon: 'error',
+  showCancelButton: true,
+  confirmButtonText: 'Yes, cancel my account.',
+  cancelButtonText: 'Nevermind, keep it.'
+})
