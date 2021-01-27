85045ea4e6d07e0716664baa16aa154e9aee2c17
Julia Frederico <magalhaesjulia@id.uff.br>
2021-01-27 14:02:41 -0300

    deleted user name migration

--- db/migrate/20210125130905_add_name_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddNameToUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :users, :first_name
-    remove_column :users, :last_name
-    add_column :users, :name, :string
-  end
-end
