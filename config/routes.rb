c6c59fb881234d440ab1813223623345815b6d65
Julia Frederico <magalhaesjulia@id.uff.br>
2021-02-03 12:26:06 -0300

    redirect to welcome after sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
-  devise_for :users, controllers: { confirmations: 'confirmations', omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  devise_for :users, controllers: { confirmations: 'confirmations', omniauth_callbacks: 'users/omniauth_callbacks', registrations: "registrations" }
+
   root to: 'pages#home'
+  get '/welcome', to: "pages#welcome"
+
   resources :users, only: [ :show, :edit, :update ]
 
   require "sidekiq/web"
