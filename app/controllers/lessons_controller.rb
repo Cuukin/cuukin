3ca479a8dd80ed421bcddf044166034761abdee3
Julia Frederico <magalhaesjulia@id.uff.br>
2021-02-11 11:41:34 -0300

    lessons#show includes skill chapters for better query

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,6 @@
 class LessonsController < ApplicationController
   def show
-    @lesson = Lesson.find(params[:id])
+    @lesson = Lesson.find(params[:id]).includes(:skill_chapters)
     @lesson_validation = LessonValidation.new
     # authorize @lesson_validation, policy_class: LessonValidationPolicy
     authorize @lesson, policy_class: LessonPolicy
