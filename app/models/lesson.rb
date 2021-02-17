3495be3b3c0c8dbc35d546d80b3d068c27deb12c
Julia Frederico <magalhaesjulia@id.uff.br>
2021-02-17 14:49:04 -0300

    commented on change to be made later

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,7 @@
 class Lesson < ApplicationRecord
   belongs_to :book
   belongs_to :recipe
-  has_one_attached :photo
+  has_one_attached :photo # change this to photo url as string later
   has_many :skill_chapters, dependent: :destroy
   has_many :skill_badges, through: :skill_chapters
   validates :name, :description, :xp, presence: true
