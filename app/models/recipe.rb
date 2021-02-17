8e5d4f6f24b6aa479aea50c4a3f01dacab02cce3
Julia Frederico <magalhaesjulia@id.uff.br>
2021-02-17 14:07:00 -0300

    commented lesson model explaining dependent destroy

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   validates :title, :description, presence: true
-  has_many :lessons
+  has_many :lessons # Can't delete a Recipe that's connected to a Lesson
+                    # 1st - connect the Lesson to a different Recipe or delete the Lesson
   has_many :recipe_methods, dependent: :destroy
   has_many :recipe_ingredients, dependent: :destroy
   has_many :recipe_tools, dependent: :destroy
