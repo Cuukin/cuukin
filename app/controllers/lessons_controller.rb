ebb1d9831d5a57a59d4dce6633fb63582f2d70f1
Julia Frederico <magalhaesjulia@id.uff.br>
2021-02-19 11:06:18 -0300

    lesson validation form working

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,7 +2,6 @@ class LessonsController < ApplicationController
   def show
     @lesson = Lesson.find(params[:id])
     @lesson_validation = LessonValidation.new
-    # authorize @lesson_validation, policy_class: LessonValidationPolicy
     authorize @lesson, policy_class: LessonPolicy
   end
 end
