294a663315ba5e1f65cfeea214e81081e08c4e94
julia frederico <magalhaesjulia@id.uff.br>
2021-02-23 19:13:02 -0300

    generate route + controller action + view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,8 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
   def show
     @book = Book.find(1) # using this instead of params id temporarily
     @lessons = Lesson.where(book_id: @book.id).includes(:recipe)
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   get '/credits', to: "pages#credits"
 
   # Journey, Book, Lesson and Lesson Validation
-  resources :books, only: [ :show ]
+  resources :books, only: [ :index, :show ]
   resources :lessons, only: [ :show ] do
     resources :lesson_validations, only: [ :create ]
   end
