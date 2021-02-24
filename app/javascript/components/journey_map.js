48796019a080df29572800742b967c1d32862ae1
julia frederico <magalhaesjulia@id.uff.br>
2021-02-24 19:10:18 -0300

    fixed media queries journey js

--- app/javascript/components/journey_map.js
+++ app/javascript/components/journey_map.js
@@ -1,16 +1,16 @@
 const journeyMap = () => {
   const journeyContainer = document.querySelector('.journey-container');
 
-  const mediaQuerySmall = window.matchMedia('(min-width: 480px)');
-  const mediaQueryXSmall = window.matchMedia('(max-width: 338px)');
+  const mediaQuerySmall = window.matchMedia('(min-width: 378px)');
+  const mediaQueryMedium = window.matchMedia('(min-width: 400px)');
 
   if (journeyContainer) {
-    // if (!mediaQuerySmall.matches) {
-    //   journeyContainer.scrollLeft = 380;
-    // } else {
-    //   journeyContainer.scrollLeft = 280;
-    // };
-    journeyContainer.scrollLeft = 380;
+    if (!mediaQuerySmall.matches) {
+      journeyContainer.scrollLeft = 480;
+    } else {
+      journeyContainer.scrollLeft = 380;
+    };
+    // journeyContainer.scrollLeft = 380;
     // window.addEventListener('load', () => {
     //   journeyContainer.scrollLeft = 380;
     // });
