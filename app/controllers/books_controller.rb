1c82ea05fe3aca9ed8f1ed347c64b2e8e85ebbf5
julia frederico <magalhaesjulia@id.uff.br>
2021-02-24 22:52:34 -0300

    refactored lesson validation check code to controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,16 @@ class BooksController < ApplicationController
     set_lessons(@book)
     authorize @lessons, policy_class: BookPolicy
 
-    set_validated_lessons
-    @validated_lessons = @lessons.select do |lesson|
-      @user_validations_lessons.include?(lesson)
+    set_validated_lessons(@lessons)
+
+    unless @validated_lessons.empty?
+      last_validated_lesson = @validated_lessons.last
+      last_validated_lesson_index = @validated_lessons.find_index(last_validated_lesson)
+      @open_lesson = [@lessons[last_validated_lesson_index + 1]]
+    end
+
+    @blocked_lessons = @lessons.reject do |lesson|
+      @validated_lessons.include?(lesson)
     end
   end
 
@@ -25,8 +32,12 @@ class BooksController < ApplicationController
     @lessons = Lesson.where(book_id: book.id).includes(:recipe)
   end
 
-  def set_validated_lessons
+  def set_validated_lessons(lessons)
     user_validations = LessonValidation.joins(:lesson).where(user: current_user).sort_by(&:lesson_id)
-    @user_validations_lessons = user_validations.map {|validation| validation.lesson}
+    user_validations_lessons = user_validations.map {|validation| validation.lesson}
+
+    @validated_lessons = lessons.select do |lesson|
+      user_validations_lessons.include?(lesson)
+    end
   end
 end
