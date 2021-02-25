08d748d1bb172131b8c961ab9fe1a24271a977fb
julia frederico <magalhaesjulia@id.uff.br>
2021-02-24 21:19:24 -0300

    complete lesson btn disabled for lessons already validated

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,6 +2,8 @@ class LessonsController < ApplicationController
   def show
     @lesson = Lesson.find(params[:id])
     @lesson_validation = LessonValidation.new
+    @user_validations = LessonValidation.joins(:lesson).where(user: current_user)
+    @user_validations = @user_validations.map {|validation| validation.lesson}
     authorize @lesson, policy_class: LessonPolicy
   end
 end
