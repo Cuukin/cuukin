7e5ba24db4db2d8fa423122e90bfca0a78ec9c04
julia frederico <magalhaesjulia@id.uff.br>
2021-02-25 15:01:03 -0300

    added locate btn to journey map

--- app/javascript/components/journey_map.js
+++ app/javascript/components/journey_map.js
@@ -1,10 +1,26 @@
 const journeyMap = () => {
   const journeyContainer = document.querySelector('.journey-container');
-  const journeyContent = document.querySelector('.journey-content');
   const journeyImg = document.querySelector('.journey-map');
+  const locateBtn = document.querySelector('#locateBtn');
 
   if (journeyContainer) {
     journeyContainer.scrollLeft = (journeyImg.scrollWidth - journeyContainer.clientWidth) / 2.4;
+  };
+
+  if (locateBtn) {
+    locateBtn.addEventListener('click', () => {
+
+      journeyContainer.scrollTo({
+        left: (journeyImg.scrollWidth - journeyContainer.clientWidth) / 2.4,
+        behavior: 'smooth'
+      });
+
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+
+    });
   };
 }
 
