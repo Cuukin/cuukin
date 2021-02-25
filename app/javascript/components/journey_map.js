60ebcdcd35f62125ce960915c54bc9d38b937978
julia frederico <magalhaesjulia@id.uff.br>
2021-02-25 00:22:02 -0300

    quick fix on icon position

--- app/javascript/components/journey_map.js
+++ app/javascript/components/journey_map.js
@@ -1,27 +1,10 @@
 const journeyMap = () => {
   const journeyContainer = document.querySelector('.journey-container');
   const journeyContent = document.querySelector('.journey-content');
-
-  // const mediaQuerySmall = window.matchMedia('(min-width: 378px)');
-  // const mediaQueryMedium = window.matchMedia('(min-width: 400px)');
+  const journeyImg = document.querySelector('.journey-map');
 
   if (journeyContainer) {
-    journeyContainer.scrollLeft = (journeyContent.scrollWidth - journeyContainer.clientWidth) / 2.4;
-
-    // if (!mediaQuerySmall.matches) {
-    //   journeyContainer.scrollLeft = 480;
-    // } else {
-    //   journeyContainer.scrollLeft = 380;
-    // };
-
-    // journeyContainer.scrollIntoView({
-    //   behavior: "smooth",
-    //   inline: "center"
-    // }); // only centers horizontally
-
-    // window.addEventListener('load', () => {
-    //   journeyContainer.scrollLeft = 380;
-    // });
+    journeyContainer.scrollLeft = (journeyImg.scrollWidth - journeyContainer.clientWidth) / 2.4;
   };
 }
 
