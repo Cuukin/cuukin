ee0496fc43b7a3d826602ebdc3ad60fc6de11ff6
julia frederico <magalhaesjulia@id.uff.br>
2021-02-25 14:04:10 -0300

    hamburger menu opens faster

--- app/javascript/components/expanded_navbar.js
+++ app/javascript/components/expanded_navbar.js
@@ -9,7 +9,7 @@ const expandedNavbar = () => {
 
     if (openBtn) {
       openBtn.addEventListener('click', () => {
-        expandedNB.style.transition = "opacity 850ms cubic-bezier(0.68, -0.6, 0.32, 1.1)";
+        expandedNB.style.transition = "opacity 180ms cubic-bezier(0.68, -0.6, 0.32, 1.1)";
         expandedNB.style.opacity = "1";
         expandedNB.style.zIndex = "10000";
         expandedNB.style.visibility = "visible";
