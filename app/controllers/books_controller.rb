5da6f56228a1c3f89e82ed985ab43f1f5dd44278
julia frederico <magalhaesjulia@id.uff.br>
2021-02-24 21:00:55 -0300

    visual feedback on which lesson is done - current - blocked

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,8 +4,29 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id]) # using this instead of params id temporarily
-    @lessons = Lesson.where(book_id: @book.id).includes(:recipe)
+    set_book
+    set_lessons(@book)
     authorize @lessons, policy_class: BookPolicy
+
+    set_validated_lessons
+    @validated_lessons = @lessons.select do |lesson|
+      @user_validations_lessons.include?(lesson)
+    end
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def set_lessons(book)
+    set_book
+    @lessons = Lesson.where(book_id: book.id).includes(:recipe)
+  end
+
+  def set_validated_lessons
+    user_validations = LessonValidation.joins(:lesson).where(user: current_user).sort_by(&:lesson_id)
+    @user_validations_lessons = user_validations.map {|validation| validation.lesson}
   end
 end
