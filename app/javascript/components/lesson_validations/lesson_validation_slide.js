c7dd06b1f6142a0ff9c223fb8c9acf8dec7b0b02
julia frederico <magalhaesjulia@id.uff.br>
2021-03-03 13:22:34 -0300

    lesson validation front

--- app/javascript/components/lesson_validations/lesson_validation_slide.js
+++ app/javascript/components/lesson_validations/lesson_validation_slide.js
@@ -1,14 +1,20 @@
 const lessonValidationSlide = () => {
   const validateLessonNextBtn = document.querySelector('#validateLessonNext');
+  const validateLessonWithoutPhotoNextBtn = document.querySelector('#validateLessonWithoutPhotoNext');
   const validateItem1 = document.querySelector('#recipe-validation-item-1');
   const validateItem2 = document.querySelector('#recipe-validation-item-2');
 
-  if (validateLessonNextBtn) {
-    validateLessonNextBtn.addEventListener('click', () => {
-      validateItem1.style.display = "none";
-      validateItem2.style.display = "block";
-    });
+  const validateFunction = (btn) => {
+    if (btn) {
+      btn.addEventListener('click', () => {
+        validateItem1.style.display = "none";
+        validateItem2.style.display = "block";
+      });
+    };
   };
+
+  validateFunction(validateLessonNextBtn);
+  validateFunction(validateLessonWithoutPhotoNextBtn);
 }
 
 export { lessonValidationSlide };
