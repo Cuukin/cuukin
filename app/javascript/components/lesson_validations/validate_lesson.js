08d6bfff2e0323dd545eb400df762e3718e30166
julia frederico <magalhaesjulia@id.uff.br>
2021-03-03 16:03:53 -0300

    complete lesson js just like complete skipped lesson

--- app/javascript/components/lesson_validations/validate_lesson.js
+++ app/javascript/components/lesson_validations/validate_lesson.js
@@ -3,13 +3,18 @@ const validateLesson = () => {
   const validateLessonBtn = document.querySelector('#validateLessonBtn');
   const closeBtn = document.querySelector('#closeValidateModal');
 
+  const validateItem1 = document.querySelector('#recipe-validation-item-1');
+  const validateItem2 = document.querySelector('#recipe-validation-item-2');
+
   if (validateLessonBtn) {
     validateLessonBtn.addEventListener('click', () => {
       validateLessonModal.style.display = "block";
+      validateItem1.style.display = "block";
     });
 
     closeBtn.addEventListener('click', () => {
       validateLessonModal.style.display = "none";
+      validateItem2.style.display = "none";
     });
   };
 }
