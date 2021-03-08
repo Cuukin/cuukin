c3f9eff1e9aa2a7c8f7317a1b57b8f858a606fd0
julia frederico <magalhaesjulia@id.uff.br>
2021-03-07 23:07:41 -0300

    updated lessons show page icons

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   has_many :lessons, dependent: :destroy
   has_many :skill_chapters, through: :lessons
 
-  enum level: { newbie: 0, chef_in_progress: 1, chef_of_party: 2, sous_chef: 3, master_chef: 4 }
+  enum level: [ "dishwasher", "newbie", "chef in progress", "chef of party", "sous chef", "master chef" ]
 
   validates :title, :description, :xp, presence: true
 end
