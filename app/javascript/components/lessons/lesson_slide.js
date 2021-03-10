3576f736cbc416a60e2e84ce0665da6bd97b8fd4
julia frederico <magalhaesjulia@id.uff.br>
2021-03-10 00:42:10 -0300

    lesson and skill videos btns on validated lessons + js wip

--- app/javascript/components/lessons/lesson_slide.js
+++ app/javascript/components/lessons/lesson_slide.js
@@ -3,6 +3,8 @@ const lessonChaptersSlide = () => {
   const lessonSlide = document.querySelector('.lesson-slide');
   const lessonChapter = document.querySelectorAll('.container-lesson-chapter');
 
+  let totalItems = document.querySelectorAll('.lesson-slide > div');
+
   const lessonOverviewContainer = document.querySelector('.container-lesson-overview');
 
   const startLessonBtn = document.querySelector('#startLesson');
@@ -10,8 +12,11 @@ const lessonChaptersSlide = () => {
   const nextBtn = document.querySelectorAll('.lesson-next-btn');
 
   const prevBtn = document.querySelector('#prev-chap-btn');
+  const skipToRecipePrevBtn = document.querySelector('#skiptorecipe-prev-btn');
 
   const startRecipe = document.querySelector('#startRecipe');
+
+  const skipToRecipe = document.querySelector('#skipToRecipe');
 
   const lessonVideos = document.querySelectorAll('.lesson-slide video');
 
@@ -21,6 +26,54 @@ const lessonChaptersSlide = () => {
 
   if (lessonOverviewContainer) {
     lessonContainer.style.display = "none";
+  };
+
+  if (skipToRecipe) {
+    skipToRecipe.addEventListener('click', () => {
+      lessonOverviewContainer.style.display = "none";
+      lessonContainer.style.display = "block";
+
+      totalItems = totalItems.length;
+      let totalRecipeItems = document.querySelectorAll('.container-lesson-recipe').length;
+      counter = totalItems - totalRecipeItems;
+
+      let progressBarWidth = 100 * (counter + 1) / totalItems;
+      progressBar.style.width = progressBarWidth + '%';
+
+      let size = lessonChapter[0].clientWidth;
+      lessonSlide.style.transition = "transform 0.4s ease-in-out";
+
+      lessonSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+      window.scrollTo({
+        top: 0
+      });
+
+      // prevBtn.style.display = "none";
+      // skipToRecipePrevBtn.style.display = "block";
+
+      // if (counter == totalItems - totalRecipeItems) {
+      //   skipToRecipePrevBtn.addEventListener('click', () => {
+      //     lessonOverviewContainer.style.display = "block";
+      //     lessonContainer.style.display = "none";
+      //     counter = 0;
+      //     prevBtn.style.display = "block";
+      //     skipToRecipePrevBtn.style.display = "none";
+      //     lessonSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+      //   });
+      // } else {
+      //   counter -= 1;
+      //   progressBarWidth = 100 * (counter + 1) / totalItems;
+      //   progressBar.style.width = progressBarWidth + '%';
+      // };
+
+      // display none on prevBtn, display block on skipRecipePrevBtn
+      // add onClick on it
+      // if (counter == totalItems - totalRecipeItems)
+      // then go back to Lesson Overview Container and set counter to 0
+      // and display block on prevBtn and none on skipRecipePrevBtn
+      // else then lessonSlide.style.transform translating with counter - 1
+    });
   };
 
   if (startLessonBtn) {
@@ -34,7 +87,7 @@ const lessonChaptersSlide = () => {
 
       lessonVideos[counter].play();
 
-      let totalItems = document.querySelectorAll('.lesson-slide > div').length;
+      totalItems = totalItems.length;
       let progressBarWidth = 100 * (counter + 1) / totalItems;
       progressBar.style.width = progressBarWidth + '%';
     });
