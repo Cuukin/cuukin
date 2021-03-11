d0b43824bcdcd879d3edaab425a804e28299f384
julia frederico <magalhaesjulia@id.uff.br>
2021-03-11 15:56:27 -0300

    added white bronze silver gold logic to user badge

--- app/models/user_badge.rb
+++ app/models/user_badge.rb
@@ -4,15 +4,19 @@ class UserBadge < ApplicationRecord
 
   validates :badge, uniqueness: { scope: :user, message: 'This user already has this badge' }
 
-  enum medal: { bronze: 0, silver: 1, gold: 2 }
+  enum medal: { white: 0, bronze: 1, silver: 2, gold: 3 }
 
   before_save :update_medal
 
   def update_medal
-    if self.xp > 200
+    if self.xp >= 50
+      self.medal = 'gold'
+    elsif self.xp >= 25
       self.medal = 'silver'
-    else
+    elsif self.xp >= 5
       self.medal = 'bronze'
+    else
+      self.medal = 'white'
     end
   end
 end
--- db/migrate/20210218193533_update_skills_tables.rb
+++ db/migrate/20210218193533_update_skills_tables.rb
@@ -2,9 +2,11 @@ class UpdateSkillsTables < ActiveRecord::Migration[6.0]
   def change
     add_reference :ingredients, :badge, foreign_key: true, null: false
     add_reference :tools, :badge, foreign_key: true, null: false
-    add_column :ingredients, :score_1, :integer
-    add_column :ingredients, :score_2, :integer
-    add_column :tools, :score_1, :integer
-    add_column :tools, :score_2, :integer
+    add_column :ingredients, :score_1, :integer, default: 0
+    add_column :ingredients, :score_2, :integer, default: 0
+    add_column :ingredients, :score_3, :integer, default: 0
+    add_column :tools, :score_1, :integer, default: 0
+    add_column :tools, :score_2, :integer, default: 0
+    add_column :tools, :score_3, :integer, default: 0
   end
 end
