58cefa97a2d45c0b311cb6b12f688eb9011a7dc1
julia frederico <magalhaesjulia@id.uff.br>
2021-03-16 20:57:19 -0300

    method index permitted on admin dashboard + recipe methods ordered by method index

--- app/admin/recipe_methods.rb
+++ app/admin/recipe_methods.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register RecipeMethod do
 
-  permit_params :title, :description, :video_url, :recipe_id
+  permit_params :title, :description, :video_url, :recipe_id, :method_index
 
 end
