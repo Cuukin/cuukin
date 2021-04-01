73d8a73598c28b95306f04264c9f64197871d215
julia frederico <magalhaesjulia@id.uff.br>
2021-04-01 17:45:19 -0300

    wip front end home carousel

--- app/javascript/components/home_carousel.js
+++ app/javascript/components/home_carousel.js
@@ -3,30 +3,31 @@ const homeCarousel = () => {
 
   // buttons
   const startCarouselBtn = document.querySelector('#registerBtn');
-  const carouselNav = document.querySelector('.home-carousel-nav');
   const nextBtn = document.querySelector('#nextBtn');
 
-  // carousel
-  const carouselSlide = document.querySelector('.home-carousel-slide');
-  const carouselItems = document.querySelectorAll('.carousel-img');
+  const carouselNav = document.querySelector('.home-carousel-nav');
+  const progressCircles = document.querySelectorAll('.circle');
 
-  const slide = document.querySelector('.home-carousel-slide');
+  // carousel
+  const carouselItems = document.querySelectorAll('.home-carousel-item');
 
   if (startCarouselBtn) {
     let counter = 0;
-    let size = carouselItems[0].clientWidth;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
 
     startCarouselBtn.addEventListener('click', () => {
       firstItem.style.display = "none";
-      slide.style.display = "flex";
       carouselNav.style.display = "block";
+      carouselItems[counter].style.display = "block";
     });
 
     nextBtn.addEventListener('click', () => {
       counter += 1;
-      carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+      carouselItems[counter].style.display = "block";
+      carouselItems[counter - 1].style.display = "none";
+      progressCircles[counter].classList.remove('circle-disabled');
+      progressCircles[counter - 1].classList.add('circle-disabled');
     });
+
   };
 }
 
