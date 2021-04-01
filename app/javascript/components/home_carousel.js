050d21b40052158cdd767602a1dd7875e9efa961
julia frederico <magalhaesjulia@id.uff.br>
2021-04-01 18:38:00 -0300

    home carousel implemented

--- app/javascript/components/home_carousel.js
+++ app/javascript/components/home_carousel.js
@@ -4,6 +4,8 @@ const homeCarousel = () => {
   // buttons
   const startCarouselBtn = document.querySelector('#registerBtn');
   const nextBtn = document.querySelector('#nextBtn');
+  const prevBtn = document.querySelector('#prevBtn');
+  const finishCarouselBtn = document.querySelector('#finishCarouselBtn');
 
   const carouselNav = document.querySelector('.home-carousel-nav');
   const progressCircles = document.querySelectorAll('.circle');
@@ -16,16 +18,60 @@ const homeCarousel = () => {
 
     startCarouselBtn.addEventListener('click', () => {
       firstItem.style.display = "none";
+
       carouselNav.style.display = "block";
+      prevBtn.style.display = "block";
+
       carouselItems[counter].style.display = "block";
+
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
     });
 
     nextBtn.addEventListener('click', () => {
       counter += 1;
+
       carouselItems[counter].style.display = "block";
       carouselItems[counter - 1].style.display = "none";
+
       progressCircles[counter].classList.remove('circle-disabled');
       progressCircles[counter - 1].classList.add('circle-disabled');
+
+      if (counter == 1) {
+        nextBtn.style.display = "none";
+        finishCarouselBtn.style.display = "block";
+      };
+
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+
+    prevBtn.addEventListener('click', () => {
+      if (counter == 0) {
+        counter = 0;
+
+        firstItem.style.display = "block";
+
+        carouselNav.style.display = "none";
+        prevBtn.style.display = "none";
+
+        carouselItems[counter].style.display = "none";
+      } else {
+        counter -= 1;
+        carouselItems[counter + 1].style.display = "none";
+        carouselItems[counter].style.display = "block";
+
+        progressCircles[counter + 1].classList.add('circle-disabled');
+        progressCircles[counter].classList.remove('circle-disabled');
+      };
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
     });
 
   };
