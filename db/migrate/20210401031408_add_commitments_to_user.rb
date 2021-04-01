3a87448b61a674df1dfd963efeb9524b6bad6f01
julia frederico <magalhaesjulia@id.uff.br>
2021-04-01 00:17:11 -0300

    added migration w/ privacy and email check + weekly commitment

---
+++ db/migrate/20210401031408_add_commitments_to_user.rb
@@ -0,0 +1,7 @@
+class AddCommitmentsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :privacy_check, :boolean, null: false, default: true
+    add_column :users, :email_check, :boolean, null: false, default: false
+    add_column :users, :weekly_commitment, :integer
+  end
+end
