30707ac12768aadefe40e566a66ef4fe469cd808
julia frederico <magalhaesjulia@id.uff.br>
2021-04-06 16:07:43 -0300

    updated migration so email check has default true

--- db/migrate/20210401031408_add_commitments_to_user.rb
+++ db/migrate/20210401031408_add_commitments_to_user.rb
@@ -1,7 +1,7 @@
 class AddCommitmentsToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :privacy_check, :boolean, null: false, default: true
-    add_column :users, :email_check, :boolean, null: false, default: false
+    add_column :users, :email_check, :boolean, null: false, default: true
     add_column :users, :weekly_commitment, :integer
   end
 end
