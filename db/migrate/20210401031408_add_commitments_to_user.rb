d66e408236f3097ec3852a4df6fef46272510f3e
julia frederico <magalhaesjulia@id.uff.br>
2021-04-06 19:31:20 -0300

    terms & conditions wip

--- db/migrate/20210401031408_add_commitments_to_user.rb
+++ db/migrate/20210401031408_add_commitments_to_user.rb
@@ -1,7 +1,8 @@
 class AddCommitmentsToUser < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :privacy_check, :boolean, null: false, default: true
-    add_column :users, :email_check, :boolean, null: false, default: true
+    add_column :users, :terms_of_service, :boolean, null: false, default: true
+    add_column :users, :email_nudges, :boolean, null: false, default: true
     add_column :users, :weekly_commitment, :integer
+    add_column :users, :motivation, :string
   end
 end
