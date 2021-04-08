b700daee5f15311c5d8ecae362cb242edbcde3da
julia frederico <magalhaesjulia@id.uff.br>
2021-04-07 21:37:58 -0300

    front end and mailer done

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,8 @@
 class UserMailer < ApplicationMailer
-  def nudge
-    # REMEMBER TO PUT (user) AS PARAMETER AGAIN BEFORE SAVING!!!
-    #@user = user
-    #@url = "https://cuukin.herokuapp.com/"
+  def nudge(user)
+    @user = user
+    @url = "https://cuukin.herokuapp.com/"
 
-    #mail(to: @user.email, subject: "Let's get Cuukin")
-    mail(to: 'julia@gmail.com')
+    mail(to: @user.email, subject: "Let's get Cuukin")
   end
 end
