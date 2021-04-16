823e7dedfc6cf4d5026c82e67524677bd34353b4
julia frederico <magalhaesjulia@id.uff.br>
2021-04-16 14:29:54 -0300

    added display names to recipe tool, recipe method and recipe technique

--- app/models/recipe_method.rb
+++ app/models/recipe_method.rb
@@ -1,6 +1,10 @@
 class RecipeMethod < ApplicationRecord
   belongs_to :recipe
 
-  validates :title, :description, presence: true # add video url here later
+  validates :title, :description, :method_index, presence: true # add video url here later
   validates :method_index, uniqueness: { scope: :recipe, message: 'Recipe already has this method index' }
+
+  def display_name
+    "#{self.recipe.title} - #{self.title}"
+  end
 end
