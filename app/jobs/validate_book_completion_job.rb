408718139dd59695703faac2ac0801b754deab66
julia frederico <magalhaesjulia@id.uff.br>
2021-04-19 22:50:16 -0300

    lesson validation rewards 50% of XP if photo isn't attached

--- app/jobs/validate_book_completion_job.rb
+++ app/jobs/validate_book_completion_job.rb
@@ -7,10 +7,19 @@ class ValidateBookCompletionJob < ApplicationJob
     lesson_validations = lesson_validations.select {|lv| lv.lesson.book_id == book.id}
 
     if lesson_validations.count == book.lessons.count
-      user.xp += lesson_validation.lesson.xp
-      user.xp += book.xp
+      if lesson_validation.photo.attached?
+        user.xp += lesson_validation.lesson.xp
+        user.xp += book.xp
+      else
+        user.xp += lesson_validation.lesson.xp / 2
+        user.xp += book.xp
+      end
     else
-      user.xp += lesson_validation.lesson.xp
+      if lesson_validation.photo.attached?
+        user.xp += lesson_validation.lesson.xp
+      else
+        user.xp += lesson_validation.lesson.xp / 2
+      end
     end
 
     user.save
