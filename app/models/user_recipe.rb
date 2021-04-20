3a26ce25babf533f76b510051797c77df9e3012e
julia frederico <magalhaesjulia@id.uff.br>
2021-04-19 23:13:01 -0300

    added lesson validation fields to user recipe

--- app/models/user_recipe.rb
+++ app/models/user_recipe.rb
@@ -2,5 +2,8 @@ class UserRecipe < ApplicationRecord
   belongs_to :recipe
   belongs_to :user
 
+  has_one_attached :photo
+  enum difficulcy: { easy: 0, medium: 1, hard: 2 }
+
   validates :recipe, uniqueness: { scope: :user, message: 'This user already has this recipe' }
 end
