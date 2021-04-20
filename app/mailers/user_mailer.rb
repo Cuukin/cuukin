d3208eccfdf7033a188f2fa696e0e25cc1191fd0
julia frederico <magalhaesjulia@id.uff.br>
2021-04-19 22:19:21 -0300

    email nudge w last lesson validated photo

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
   def nudge(user)
     @user = user
+    @photo = LessonValidation.where(user: user).last.recipe.photo_url
     @url = "https://cuukin.herokuapp.com/"
 
     mail(to: @user.email, subject: "Let's get Cuukin")
