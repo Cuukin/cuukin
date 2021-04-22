1ce2e2282dd9362fa27dc0c94ef5bc27ec2114eb
julia frederico <magalhaesjulia@id.uff.br>
2021-04-22 17:06:33 -0300

    commented user badges and user awards controllers index action

--- app/controllers/user_awards_controller.rb
+++ app/controllers/user_awards_controller.rb
@@ -1,7 +1,7 @@
 class UserAwardsController < ApplicationController
   def index
-    @user_awards = UserAward.where(user_id: current_user.id).includes(:award)
-    authorize @user_awards, policy_class: UserAwardPolicy
+    # @user_awards = UserAward.where(user_id: current_user.id).includes(:award)
+    # authorize @user_awards, policy_class: UserAwardPolicy
   end
 
   def show
--- app/controllers/user_badges_controller.rb
+++ app/controllers/user_badges_controller.rb
@@ -1,7 +1,7 @@
 class UserBadgesController < ApplicationController
   def index
-    @user_badges = UserBadge.where(user_id: current_user.id).includes(:badge)
-    authorize @user_badges, policy_class: UserBadgePolicy
+    # @user_badges = UserBadge.where(user_id: current_user.id).includes(:badge)
+    # authorize @user_badges, policy_class: UserBadgePolicy
   end
 
   def show
