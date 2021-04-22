f8f8841072f13766b6178669851b134f4b29adbd
julia frederico <magalhaesjulia@id.uff.br>
2021-04-22 16:41:08 -0300

    removed recipe method ingredients method

--- app/models/recipe_method_ingredient.rb
+++
@@ -1,6 +0,0 @@
-class RecipeMethodIngredient < ApplicationRecord
-  belongs_to :recipe_method
-  belongs_to :recipe_ingredient
-
-  validates :recipe_method, uniqueness: { scope: :recipe_ingredient, message: 'This method already has this recipe ingredient' }
-end
