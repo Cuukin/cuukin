787ecffc046c196aba0b2017f6349feaa12a932d
julia frederico <magalhaesjulia@id.uff.br>
2021-04-23 15:46:09 -0300

    lesson overview wip

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,11 +2,11 @@ class LessonsController < ApplicationController
   before_action :set_lesson, :set_validations
 
   def show
-    validated?
-    skipped?
     first?
-    open?
-    open_to_unblock?
+    validated?
+    available?
+    skipped?
+    locked?
 
     @lesson_validation = LessonValidation.new
     @lesson_unblock = LessonUnblock.new
@@ -16,41 +16,36 @@ class LessonsController < ApplicationController
 
   private
 
-  def validated?
-    validated_lessons = @validated_lessons.select {|val| val.validated}
-    validated_lessons = validated_lessons.map {|val| val.lesson}
-    @validated = validated_lessons.include?(@lesson)
+  def locked?
+    @locked = !(@available || @first || @true)
   end
 
-  def skipped?
-    skipped_lessons = @validated_lessons.reject {|val| val.validated}
-    skipped_lessons = skipped_lessons.map {|val| val.lesson}
-    @skipped = skipped_lessons.include?(@lesson)
+  def validated?
+    @validated = @validated_lessons.include?(@lesson)
   end
 
   def first?
     @first = (@book.lessons.first == @lesson)
   end
 
-  def open?
-    validated_lessons = @validated_lessons.map {|val| val.lesson}
-    prev_lesson = @book.lessons.select {|l| l.id == (@lesson.id - 1)}
-    @open = validated_lessons.include?(prev_lesson.first)
+  def available?
+    prev_lesson_id = @lesson.id - 1
+    unless prev_lesson_id == 0
+      @available = @validated_lessons.include?(Lesson.find(prev_lesson_id))
+    end
   end
 
-  def open_to_unblock?
-    validated_lessons = @validated_lessons.map {|val| val.lesson}
-    prev_lesson = @book.lessons.select {|l| l.id == (@lesson.id - 2)}
-    is_second = (@book.lessons[1].id == @lesson.id)
-    @open_to_unblock = is_second || validated_lessons.include?(prev_lesson.first)
+  def skipped?
+    @skipped = @skipped_lessons.include?(@lesson)
   end
 
   def set_validations
-    @validated_lessons = LessonValidation.includes(:lesson).where(user: current_user)
+    @validated_lessons = LessonValidation.includes(:lesson).where(user: current_user, validated: true).map {|val| val.lesson}
+    @skipped_lessons = LessonValidation.includes(:lesson).where(user: current_user, validated: false).map {|val| val.lesson}
   end
 
   def set_lesson
-    @lesson = Lesson.includes(:recipe).find(params[:id])
+    @lesson = Lesson.find(params[:id])
     @book = @lesson.book
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,6 +10,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_nutritional_infos, dependent: :destroy
   has_many :lesson_recipes
   has_many :lessons, through: :lesson_recipes
+  has_many :user_recipes
 
   has_many :ingredients, through: :recipe_ingredients
   has_many :tools, through: :recipe_tools
