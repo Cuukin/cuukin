5e7a2c707da8f7a1427d26a0d2f6726c41a89cff
julia frederico <magalhaesjulia@id.uff.br>
2021-04-23 23:30:16 -0300

    wip page scroll

---
+++ app/javascript/components/lessons/lesson_referrers.js
@@ -0,0 +1,13 @@
+const lessonReferrers = () => {
+  // const prevRecipe = document.querySelector('#prevRecipe');
+  // const validation = document.querySelector('.lesson-overview-validation');
+
+  // if (prevRecipe && validation) {
+  //   const top = validation.offsetTop;
+  //   window.addEventListener('load', (event) => {
+  //     window.scrollTo({top: 1000});
+  //   });
+  // };
+};
+
+export { lessonReferrers };
