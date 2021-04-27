6268e5189d338d0d13e3046cc78f6379ac628028
julia frederico <magalhaesjulia@id.uff.br>
2021-04-26 23:52:23 -0300

    skill chapter progression cleared out of bugs

--- app/javascript/components/lessons/skill_chapters.js
+++ app/javascript/components/lessons/skill_chapters.js
@@ -6,25 +6,25 @@ const skillChapters = () => {
     let skillChecks = document.querySelectorAll('.nav-check');
     const skillsBtns = document.querySelectorAll('.open-skill-btn');
 
-    const prevBtn = document.querySelector('#prevChapter');
-    const nextBtn = document.querySelector('#nextChapter');
+    const allCards = document.querySelectorAll('.skill-chapter-card');
+    const cardsQuantity = allCards.length;
 
-    const cards = document.querySelectorAll('.skill-chapter-card');
-    const cardsQuantity = cards.length;
+    let cards = [];
+    let firstCard = [];
 
+    let cardIndex = 0;
     let counter = 0;
 
-    const backToOverview = (btn) => {
-      btn.addEventListener('click', () => {
-        lessonOverview.style.display = "block";
-        skillsContainer.style.display = "none";
-        cards.forEach((card) => {
-          card.style.display = "none";
-          let check = skillChecks[card.classList[1]];
-          check.querySelector('#skillDone').style.display = "none";
-          check.querySelector('#play').style.display = "initial";
-        });
-      })
+    const backToOverview = () => {
+      lessonOverview.style.display = "block";
+      skillsContainer.style.display = "none";
+      counter = 0;
+      allCards.forEach((card) => {
+        card.style.display = "none";
+        let check = skillChecks[card.classList[1]];
+        check.querySelector('#skillDone').classList.add('skill-none');
+        check.querySelector('#play').classList.remove('skill-none');
+      });
     };
 
     skillsBtns.forEach((btn) => {
@@ -32,78 +32,67 @@ const skillChapters = () => {
       let card = document.getElementById(`skillCard-${id}`);
 
       btn.addEventListener('click', () => {
+        counter = 0;
+
         lessonOverview.style.display = "none";
         skillsContainer.style.display = "block";
         card.style.display = "block";
-        counter = parseInt(card.classList[1]);
-        let check = skillChecks[counter];
-        check.querySelector('#skillDone').style.display = "initial";
-        check.querySelector('#play').style.display = "none";
 
-        // if (counter > 0) {
-        //   skillChecks = Array.prototype.slice.call(skillChecks);
-        //   let checks = skillChecks.slice(0, counter + 1);
-        //   console.log(counter);
-        //   console.log(checks);
-        //   checks.forEach((check) => {
-        //     check.querySelector('#done').style.display = "block";
-        //     check.querySelector('#play').style.display = "none";
-        //   });
-        // } else {
-        //   let check = skillChecks[counter];
-        //   check.querySelector('#done').style.display = "block";
-        //   check.querySelector('#play').style.display = "none";
-        // }
+        skillChecks[counter].querySelector('#skillDone').classList.remove('skill-none');
+        skillChecks[counter].querySelector('#play').classList.add('skill-none');
+
+        cards = Array.prototype.slice.call(document.querySelectorAll('.skill-chapter-card'));
+        cardIndex = parseInt(card.classList[1]);
+        firstCard = cards.splice(cardIndex, 1);
       });
     });
+
+    const prevBtn = document.querySelector('#prevChapter');
+    const nextBtn = document.querySelector('#nextChapter');
 
     if (cardsQuantity > 1) {
       nextBtn.addEventListener('click', () => {
         counter += 1;
 
-        if (counter < cardsQuantity) {
-          cards[counter].style.display = "block";
-          cards[counter - 1].style.display = "none";
-          let check = skillChecks[counter];
-          check.querySelector('#skillDone').style.display = "initial";
-          check.querySelector('#play').style.display = "none";
+        if (counter == 1) {
+          firstCard[0].style.display = "none";
+          cards[counter - 1].style.display = "block";
+          skillChecks[counter].querySelector('#skillDone').classList.remove('skill-none');
+          skillChecks[counter].querySelector('#play').classList.add('skill-none');
+        } else if (counter == cardsQuantity) {
+          backToOverview();
         } else {
-          lessonOverview.style.display = "block";
-          skillsContainer.style.display = "none";
-          cards.forEach((card) => {
-            card.style.display = "none";
-            let check = skillChecks[card.classList[1]];
-            check.querySelector('#skillDone').style.display = "none";
-            check.querySelector('#play').style.display = "initial";
-          });
-          counter = 0;
+          cards[counter - 1].style.display = "block";
+          cards[counter - 2].style.display = "none";
+          skillChecks[counter].querySelector('#skillDone').classList.remove('skill-none');
+          skillChecks[counter].querySelector('#play').classList.add('skill-none');
         };
       });
 
       prevBtn.addEventListener('click', () => {
-        if (counter > 0) {
-          counter -= 1;
-          cards[counter].style.display = "block";
-          cards[counter + 1].style.display = "none";
-          let check = skillChecks[counter];
-          check.querySelector('#skillDone').style.display = "initial";
-          check.querySelector('#play').style.display = "none";
+        counter -= 1;
+
+        if (counter == 0) {
+          firstCard[0].style.display = "block";
+          cards[counter].style.display = "none";
+          skillChecks[counter + 1].querySelector('#skillDone').classList.add('skill-none');
+          skillChecks[counter + 1].querySelector('#play').classList.remove('skill-none');
+        } else if (counter == -1) {
+          backToOverview();
         } else {
-          lessonOverview.style.display = "block";
-          skillsContainer.style.display = "none";
-          cards.forEach((card) => {
-            card.style.display = "none";
-            let check = skillChecks[card.classList[1]];
-            check.querySelector('#skillDone').style.display = "none";
-            check.querySelector('#play').style.display = "initial";
-          });
-          counter = 0;
+          cards[counter].style.display = "none";
+          skillChecks[counter + 1].querySelector('#skillDone').classList.add('skill-none');
+          skillChecks[counter + 1].querySelector('#play').classList.remove('skill-none');
+          cards[counter - 1].style.display = "block";
         };
       });
     } else {
-      backToOverview(prevBtn);
-      backToOverview(nextBtn);
-      counter = 0;
+      prevBtn.addEventListener('click', (event) => {
+        backToOverview();
+      });
+      nextBtn.addEventListener('click', (event) => {
+        backToOverview();
+      });
     };
   };
 };
