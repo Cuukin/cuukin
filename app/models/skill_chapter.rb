c8d097a503a6e39f3d724e2e2f81ad4b9ff95edf
julia frederico <magalhaesjulia@id.uff.br>
2021-04-27 14:41:36 -0300

    prev - next btn + help btn with 48px height

--- app/models/skill_chapter.rb
+++ app/models/skill_chapter.rb
@@ -3,6 +3,7 @@ class SkillChapter < ApplicationRecord
   belongs_to :badge
 
   has_many :user_skills, dependent: :destroy
+  has_many :users, through: :user_skills
 
   validates :title, presence: true
 end
