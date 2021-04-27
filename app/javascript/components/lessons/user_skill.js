20da3b701275b2fba6080ad9f135dd16f3f25498
julia frederico <magalhaesjulia@id.uff.br>
2021-04-27 16:29:27 -0300

    user skills check update on lesson overview after user plays vid + recipe overview only shows tool/ingredient/nutri if not empty

--- app/javascript/components/lessons/user_skill.js
+++ app/javascript/components/lessons/user_skill.js
@@ -1,12 +1,14 @@
 const userSkill = () => {
   const userSkillBtns = document.querySelectorAll('.invisible-skill-btn');
-  const skillVideos = document.querySelectorAll('.skillVideo');
+  //const skillVideos = document.querySelectorAll('.skillVideo');
 
   if (userSkillBtns) {
     userSkillBtns.forEach((btn) => {
       let video = document.querySelector(`.${btn.id}`);
+      let tag = document.getElementById(`${video.classList[2]}`);
       video.addEventListener('play', () => {
         btn.click();
+        tag.querySelector('.skill-check').classList.remove('skill-none');
       })
     });
   };
