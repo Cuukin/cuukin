88d21c7cd058f77952b0b6f2ce097d253762e2e8
julia frederico <magalhaesjulia@id.uff.br>
2021-04-28 13:11:59 -0300

    added optional to recipe tools

--- app/admin/recipe_tools.rb
+++ app/admin/recipe_tools.rb
@@ -1,13 +1,14 @@
 ActiveAdmin.register RecipeTool do
   menu parent: "Recipe"
 
-  permit_params :recipe_id, :tool_id, :recipe_methods
+  permit_params :recipe_id, :tool_id, :optional, :recipe_methods
 
   index do
     selectable_column
     # column :id
     column :recipe
     column :tool
+    column :optional
     column :recipe_methods
     actions
   end
--- app/admin/recipes.rb
+++ app/admin/recipes.rb
@@ -1,14 +1,13 @@
 ActiveAdmin.register Recipe do
   menu parent: "Recipe"
 
-  permit_params :title, :description, :prep_time, :photo_url, :external_url
+  permit_params :title, :description, :prep_time, :active_time, :passive_time, :photo_url, :external_url
 
   index do
     selectable_column
     column :id
     column :title
     column :prep_time
-    column :photo_url
     column :external_url
     actions
   end
