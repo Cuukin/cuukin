f30b0c02b3ab51bb48ff504019a4475d74904a1f
julia frederico <magalhaesjulia@id.uff.br>
2021-04-30 19:39:45 -0300

    grocery list show page html structure implemented

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -3,6 +3,8 @@ class GroceryListsController < ApplicationController
 
   def show
     authorize @grocery_list, policy_class: GroceryListPolicy
+    # @items = @grocery_list.grocery_list_items.includes(:ingredients).includes(:badges).order("ingredients.badge_id desc")
+    #@items = items.includes(:ingredient).order("ingredients.badge desc")
   end
 
   private
