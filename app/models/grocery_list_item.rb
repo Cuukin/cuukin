0e547761453319a4f62e9ba59cfa97ec1126324b
julia frederico <magalhaesjulia@id.uff.br>
2021-04-30 14:22:32 -0300

    added user uniqueness validationt o grocery list model

--- app/models/grocery_list_item.rb
+++ app/models/grocery_list_item.rb
@@ -1,5 +1,7 @@
 class GroceryListItem < ApplicationRecord
   belongs_to :grocery_list
   belongs_to :ingredient
+
   validates :quantity, :unit, presence: true
+  validates :user, uniqueness: true
 end
