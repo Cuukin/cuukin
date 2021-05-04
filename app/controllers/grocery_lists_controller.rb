fb89cff34c3b007038e86eebf328ddaec15e16a9
julia frederico <magalhaesjulia@id.uff.br>
2021-05-03 23:38:37 -0300

    update method to only redirect if params are different than property itself

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -9,9 +9,7 @@ class GroceryListsController < ApplicationController
 
   def update
     authorize @grocery_list, policy_class: GroceryListPolicy
-    if grocery_list_params["scheduled_reminder"] == @grocery_list.scheduled_reminder
-      # redirect_to grocery_list_path(@grocery_list)
-    else
+    unless grocery_list_params["scheduled_reminder"] == @grocery_list.scheduled_reminder
       @grocery_list.update(grocery_list_params)
       scheduled_time = @grocery_list.scheduled_reminder
       GroceryListReminderJob.set(wait_until: scheduled_time).perform_later(@grocery_list, scheduled_time) if scheduled_time
