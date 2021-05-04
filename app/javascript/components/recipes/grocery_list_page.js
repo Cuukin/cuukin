2470791f7f5f087674f076206baa838689d4f1dc
julia frederico <magalhaesjulia@id.uff.br>
2021-05-03 21:21:39 -0300

    copy to clipboard button

--- app/javascript/components/recipes/grocery_list_page.js
+++ app/javascript/components/recipes/grocery_list_page.js
@@ -44,6 +44,24 @@ const groceryListPage = () => {
       reminderForm.classList.remove('d-none');
     });
   };
+
+  const copyList = document.querySelector('.tag-btn');
+  if (copyList) {
+    copyList.addEventListener('click', (event) => {
+      const el = document.createElement('textarea');
+      let strs = document.querySelectorAll('tr');
+      strs.forEach((str) => {
+        if (str.style.display === 'none') {
+        } else {
+          el.value = `${el.value}\n${str.innerText}`
+        };
+      });
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+    });
+  };
 };
 
 export { groceryListPage };
