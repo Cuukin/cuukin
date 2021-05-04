251e67dfd292ef8fc7ba1ed8fada207f428ca7a4
julia frederico <magalhaesjulia@id.uff.br>
2021-05-03 21:50:01 -0300

    tweaks on grocery list reminder job

--- app/jobs/grocery_list_reminder_job.rb
+++ app/jobs/grocery_list_reminder_job.rb
@@ -3,10 +3,12 @@ class GroceryListReminderJob < ApplicationJob
 
   def perform(grocery_list, scheduled_time)
     if scheduled_time == grocery_list.scheduled_reminder && !grocery_list.grocery_list_items.empty?
-      GroceryMailer.grocery_list(grocery_list).deliver_now
-      grocery_list.grocery_list_items.each {|item| item.destroy }
-      grocery_list.scheduled_reminder = nil
-      grocery_list.save
+      GroceryMailer.grocery_list(grocery_list).deliver_later
+      if Time.now > scheduled_time
+        grocery_list.grocery_list_items.each {|item| item.destroy }
+        grocery_list.scheduled_reminder = nil
+        grocery_list.save
+      end
     end
   end
 end
