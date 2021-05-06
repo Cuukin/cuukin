048c24d4b09b59bc8d5e2d801dc51b216712a37f
julia frederico <magalhaesjulia@id.uff.br>
2021-05-06 16:19:25 -0300

    public vs friends feed toggle btn working

--- app/javascript/components/toggle_friends.js
+++ app/javascript/components/toggle_friends.js
@@ -1,9 +1,23 @@
 const toggleFriends = () => {
-  const friendsToggle = document.querySelector('.friends-toggle');
+  const toggleBtn = document.querySelector('.friends-toggle');
+  const friendsFeed = document.querySelector('.friends-feed-container');
+  const publicFeed = document.querySelector('.public-feed-container');
 
-  if (friendsToggle) {
-    friendsToggle.addEventListener('click', (event) => {
-      const items = friendsToggle.querySelectorAll('.feed');
+  if (toggleBtn) {
+    toggleBtn.addEventListener('click', (event) => {
+      const friendsToggle = document.getElementById('friendsToggle');
+      const publicToggle = document.getElementById('publicToggle');
+
+      if (friendsToggle.classList[1] == 'feedSelected') {
+        publicFeed.classList.toggle('d-none');
+        friendsFeed.classList.toggle('d-none');
+      } else {
+        publicFeed.classList.toggle('d-none');
+        friendsFeed.classList.toggle('d-none');
+      };
+
+      const items = toggleBtn.querySelectorAll('.feed');
+
       items.forEach((item) => {
         item.classList.toggle('feedSelected');
       });
