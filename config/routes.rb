4343d0c17e7b68620dd1e141a2457c8eed27dcf0
julia frederico <magalhaesjulia@id.uff.br>
2021-05-07 17:09:58 -0300

    removed edit route from user recipes

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
 
   resources :lesson_validations, only: [ :show ]
   resources :recipes, only: [ :show ]
-  resources :user_recipes, only: [ :index, :edit, :update ] do
+  resources :user_recipes, only: [ :index, :update ] do
     member do
       patch 'archive', to: 'user_recipes#archive'
       put 'archive', to: 'user_recipes#archive'
