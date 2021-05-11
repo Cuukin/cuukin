64d79bac3f927336a0f41e9e37359354822dd093
julia frederico <magalhaesjulia@id.uff.br>
2021-05-11 13:54:00 -0300

    visual explaining of back and forward double tap function if its user first time watching a skill chapter

--- app/javascript/components/lessons/video_player.js
+++ app/javascript/components/lessons/video_player.js
@@ -6,11 +6,21 @@ const videoPlayer = () => {
       let video = container.querySelector('.skillVideo');
       let backwardBtn = container.querySelector('#backward10');
       let forwardBtn = container.querySelector('#forward10');
+
       backwardBtn.addEventListener('dblclick', (event) => {
         video.currentTime -= 10;
+        if (backwardBtn.classList.contains('explain-video-control-backward')) {
+          backwardBtn.classList.remove('explain-video-control-backward');
+          forwardBtn.classList.remove('explain-video-control-forward');
+        };
       });
+
       forwardBtn.addEventListener('dblclick', (event) => {
         video.currentTime += 10;
+        if (forwardBtn.classList.contains('explain-video-control-forward')) {
+          forwardBtn.classList.remove('explain-video-control-forward');
+          backwardBtn.classList.remove('explain-video-control-backward');
+        };
       });
     });
   };
