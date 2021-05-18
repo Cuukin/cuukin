c503d6da73cebb92e3f7ab9da0639ad8d6e5b1ff
julia frederico <magalhaesjulia@id.uff.br>
2021-05-17 22:03:28 -0300

    javascript interaction w/ form re factored for user recipes

--- app/javascript/components/recipes/recipe_search.js
+++ app/javascript/components/recipes/recipe_search.js
@@ -1,28 +1,34 @@
 const recipeSearch = () => {
-  const completedRecipesContainer = document.querySelector('#completedRecipes');
-  const unlockedRecipesContainer = document.querySelector('#unlockedRecipes');
+  const cuukbookContainer = document.querySelector('.recipes-container');
+  const completedContainer = document.querySelector('.cuukbook-complete-container');
+  const searchedContainer = document.querySelector('.cuukbook-search-container');
 
-  const unlockedBtn = document.querySelector('#unlockedBtn');
-  const completedBtn = document.querySelector('#completedBtn');
+  if (completedContainer) {
+    let searchForm = document.querySelector('#searchForm')
+    let searchInput = searchForm.querySelector('.search-form-control');
+    let searchBtn = searchForm.querySelector('#searchBtn');
 
-  if (completedRecipesContainer) {
-    const searchRecipesContainer = document.querySelector('#searchedRecipes');
-    const searchInput = document.querySelector('.search-form-control');
-    const searchBtn = document.querySelector('#searchRecipe');
-
-    document.addEventListener('keyup', (event) => {
-      searchBtn.click();
-
-      if (searchInput.value == "") {
-        searchRecipesContainer.classList.add('d-none');
-
-        if (completedBtn.classList.contains('recipe-navbar-active')) {
-          completedRecipesContainer.classList.remove('d-none');
-        } else {
-          unlockedRecipesContainer.classList.remove('d-none');
-        };
+    searchBtn.addEventListener('click', (event) => {
+      if (!(searchInput.value == "") && (searchBtn.value == "Search")) {
+        searchBtn.value = "Clear";
+      } else if (searchBtn.value == "Clear") {
+        searchBtn.value = "Search";
+        searchInput.value = "";
+        completedContainer.classList.remove('d-none');
+        searchedContainer.classList.add('d-none');
       };
     });
+
+    // document.addEventListener('keyup', (event) => {
+    //   // event.preventDefault();
+    //   searchBtn.click();
+    //   // searchForm.submit();
+
+    //   if (searchInput.value == "") {
+    //     searchedContainer.classList.add('d-none');
+    //     completedContainer.classList.remove('d-none');
+    //   };
+    // });
   };
 };
 
