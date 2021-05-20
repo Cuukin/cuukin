9f942a9d99dbf224635de753a62b335d0da15423
julia frederico <magalhaesjulia@id.uff.br>
2021-05-19 21:01:04 -0300

    grocery list reminder converting local time to real time in utc

--- app/jobs/grocery_list_reminder_job.rb
+++ app/jobs/grocery_list_reminder_job.rb
@@ -3,7 +3,7 @@ class GroceryListReminderJob < ApplicationJob
 
   def perform(grocery_list, list_items, scheduled_time)
     GroceryMailer.grocery_list(list_items, grocery_list.user).deliver_later
-    if Time.now >= scheduled_time
+    if Time.now.utc >= scheduled_time
       grocery_list.scheduled_reminder = nil
       grocery_list.save
     end
