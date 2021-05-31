db9ab837e9dbf071e8d6085fd153fdd78779df6e
julia frederico <magalhaesjulia@id.uff.br>
2021-05-31 17:10:24 -0300

    after create on quiz result - check if answer is correct to transfer xp

--- app/models/quiz_result.rb
+++ app/models/quiz_result.rb
@@ -3,6 +3,18 @@ class QuizResult < ApplicationRecord
   belongs_to :quiz_question
   belongs_to :quiz_question_option
 
-  validates :is_correct, presence: true
   validates :quiz_question, uniqueness: { scope: :user, message: 'This question has already been answered' }
+
+  after_create :set_result
+
+  private
+
+  def set_result
+    if self.quiz_question_option.is_correct
+      self.is_correct = true
+      self.save
+      self.user.xp += self.quiz_question.xp
+      self.user.save
+    end
+  end
 end
