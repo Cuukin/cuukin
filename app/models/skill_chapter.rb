ed4fb87d12325c03962f48deaf5690e3ec818d2b
julia frederico <magalhaesjulia@id.uff.br>
2021-06-01 17:19:10 -0300

    quiz question partial rendering questions related to skill chapter

--- app/models/skill_chapter.rb
+++ app/models/skill_chapter.rb
@@ -4,6 +4,7 @@ class SkillChapter < ApplicationRecord
 
   has_many :user_skills, dependent: :destroy
   has_many :users, through: :user_skills
+  has_many :quiz_questions
 
   validates :title, presence: true
 end
