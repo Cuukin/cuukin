1ec2bae3b8b1bd28d8d4214e70b78694d99fade0
julia frederico <magalhaesjulia@id.uff.br>
2021-06-02 19:51:54 -0300

    transition cuukies animation implemented

--- app/javascript/components/lessons/quiz_result.js
+++ app/javascript/components/lessons/quiz_result.js
@@ -19,10 +19,28 @@ const quizResult = () => {
         let answer = quiz.querySelector('.quiz-answer-content');
         let correctOption = quiz.querySelector("[data-answer='1']");
 
+        let cuukiesContainer = quiz.querySelector('.transition-cuukies');
+
         // functions (spend cuukie or gain xp)
 
         const spendCuukies = () => {
-
+          setTimeout(function() {
+            cuukiesContainer.classList.remove('d-none');
+          }, 0);
+          setTimeout(function() {
+            cuukiesContainer.style.transition = 'all ease-in-out .7s';
+            cuukiesContainer.style.opacity = '1';
+            cuukiesContainer.style.bottom = '440px';
+          }, 50);
+          setTimeout(function() {
+            cuukiesContainer.style.opacity = '0';
+            cuukiesContainer.style.bottom = '480px';
+          }, 800);
+          setTimeout(function() {
+            cuukies -= 1;
+            userCuukies.dataset.userCurrency = cuukies;
+            nbCuukie.innerText = cuukies;
+          }, 900);
         };
 
         const gainXp = () => {
@@ -38,9 +56,7 @@ const quizResult = () => {
             answer.classList.remove('d-none');
             next.classList.remove('d-none');
             quiz.dataset.quizCompleted = "true";
-            cuukies -= 1;
-            userCuukies.dataset.userCurrency = cuukies;
-            nbCuukie.innerText = cuukies;
+            spendCuukies();
           } else {
             let error = '<p class="quiz-error">Not enough cuukies</p>'
             quiz.querySelector('.quiz-card').insertAdjacentHTML('beforeend', error);
