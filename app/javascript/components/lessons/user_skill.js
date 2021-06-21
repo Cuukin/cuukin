0e4ada9be832abd85fa221de5a0609019eb6f3f7
julia frederico <magalhaesjulia@id.uff.br>
2021-06-21 18:34:20 -0300

    gain xp animation on skill video

--- app/javascript/components/lessons/user_skill.js
+++ app/javascript/components/lessons/user_skill.js
@@ -12,15 +12,35 @@ const userSkill = () => {
       let nbXp = document.querySelector('#nb-xp p');
       let userXp = parseInt(nbXp.innerText);
 
+      let xpContainer = document.querySelector('.transition-xp');
+
+      const gainXp = () => {
+        setTimeout(function() {
+          xpContainer.classList.remove('d-none');
+        }, 0);
+        setTimeout(function() {
+          xpContainer.style.transition = 'all ease-in-out .7s';
+          xpContainer.style.opacity = '1';
+          xpContainer.style.top = '20px';
+        }, 50);
+        setTimeout(function() {
+          xpContainer.style.opacity = '0';
+          xpContainer.style.top = '-10px';
+        }, 800);
+        setTimeout(function() {
+          userXp = parseInt(nbXp.innerText);
+          nbXp.innerText = userXp + 10;
+        }, 950);
+      };
+
       video.addEventListener('play', () => {
         setTimeout(function() {
           if (!video.paused && card.dataset.videoCompleted == "false") {
             btn.click();
             tag.querySelector('.skill-check').classList.remove('d-none');
             card.dataset.videoCompleted = "true";
-            userXp = parseInt(nbXp.innerText);
-            nbXp.innerText = userXp + 10;
             btn.remove();
+            gainXp();
           };
         }, 20000);
 
