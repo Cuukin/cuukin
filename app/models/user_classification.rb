d67803de0f97ec5b501cc2461b83bd9c925b2adf
julia frederico <magalhaesjulia@id.uff.br>
2021-06-28 19:16:10 -0300

    user classifcations as integer pure not enum

--- app/models/user_classification.rb
+++ app/models/user_classification.rb
@@ -2,7 +2,7 @@ class UserClassification < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
 
-  enum classification: { dislike: 0, like: 1, love: 2 }
+  # enum classification: { dislike: 0, like: 1, love: 2 }
   validates :classification, presence: true
 
   validate :must_be_bbc_recipe
